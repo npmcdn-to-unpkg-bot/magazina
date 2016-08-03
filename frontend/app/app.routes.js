f84873fb5fda8a31ca6ad8335ff02ac4e975c3e0
bodpad <myqoop@yandex.ru>
2016-08-03 22:47:59 +0300

    Переработан toolbar, добавлена страница wiki

--- frontend/app/app.routes.js
+++ frontend/app/app.routes.js
@@ -6,6 +6,7 @@ var hero_detail_component_1 = require('./hero-detail.component');
 var checkout_component_1 = require('./checkout.component');
 var product_search_component_1 = require('./product-search.component');
 var product_detail_component_1 = require('./product-detail.component');
+var wiki_component_1 = require('./wiki.component');
 var routes = [
     { path: '', component: product_search_component_1.ProductSearchComponent },
     { path: 'heroes', component: heroes_component_1.HeroesComponent },
@@ -14,6 +15,7 @@ var routes = [
     { path: 'detail/:id', component: hero_detail_component_1.HeroDetailComponent },
     { path: 'checkout', component: checkout_component_1.CheckoutComponent },
     { path: 'sku/:id', component: product_detail_component_1.ProductDetailComponent },
+    { path: 'wiki', component: wiki_component_1.WikiComponent },
 ];
 exports.appRouterProviders = [
     router_1.provideRouter(routes)
--- frontend/app/app.routes.ts
+++ frontend/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { CheckoutComponent } from './checkout.component';
 import { ProductSearchComponent } from './product-search.component';
 import { ProductDetailComponent } from './product-detail.component';
+import { WikiComponent } from './wiki.component';
 
 const routes: RouterConfig = [
     { path: '', component: ProductSearchComponent },
@@ -14,6 +15,7 @@ const routes: RouterConfig = [
     { path: 'detail/:id', component: HeroDetailComponent },
     { path: 'checkout', component: CheckoutComponent },
     { path: 'sku/:id', component: ProductDetailComponent },
+    { path: 'wiki', component: WikiComponent },
 ];
 
 export const appRouterProviders = [
